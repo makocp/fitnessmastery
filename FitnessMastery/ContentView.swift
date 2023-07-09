f5b3641d2517da09c1e90ca0a5ee84dedf509f9b
makocp <mako.codeproducer@gmail.com>
2023-07-09 19:02:42 +0200

    views for choosing workout incl. navigation added

--- FitnessMastery/ContentView.swift
+++ FitnessMastery/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let workouts = [
+        Workout(title: "Chest Easy", sets: [Set(amount: 50, setName: "Push-Up"), Set(amount: 50, setName: "Dip")]),
+        Workout(title: "Chest Medium", sets: [Set(amount: 100, setName: "Push-Up"), Set(amount: 100, setName: "Dip")]),
+        Workout(title: "Back Medium", sets: [Set(amount: 50, setName: "Pull-Up")]),
+        Workout(title: "Back Hard", sets: [Set(amount: 100, setName: "Pull-Up")]),
+        Workout(title: "Core Medium", sets: [Set(amount: 100, setName: "Crunch"), Set(amount: 100, setName: "Side Crunch")]),
+    ]
+    
     var body: some View {
-        VStack {
-            
-            
+        NavigationView {
+            List {
+                ForEach(workouts) { workout in
+                    NavigationLink(destination: WorkoutStartView(workout: workout)){
+                        WorkoutRowView(workout: workout)
+                    }
+                }
+            }
+            .navigationTitle("Choose a workout")
         }
-        .padding()
     }
 }
 
---
+++ FitnessMastery/ui/WorkoutStartView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutStartView.swift
+//  FitnessMastery
+//
+//  Created by Mako CP on 09.07.23.
+//
+
+import SwiftUI
+
+struct WorkoutStartView: View {
+    var workout: Workout
+    
+    var body: some View {
+        VStack {
+            Text("Navigation works!")
+        }
+    }
+}
